5da00f3e33535fc8ef4c1aa8ae4d9d8d75e35c8d
jittergtz <sandro.gantze@icloud.com>
2024-04-08 16:22:02 +0200

    deploy #4

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -44,7 +44,7 @@ export default async function ProjectsPage() {
         </div>
         <div className="w-full h-px bg-zinc-800" />
 
-        <div className="grid grid-cols-1 border gap-8 mx-auto lg:grid-cols-2 ">
+        <div className="grid grid-cols-1  gap-8 mx-auto lg:grid-cols-2 ">
           <Card>
             <Link href={`/projects/${featured.slug}`}>
               <article className="relative w-full h-full p-4 md:p-6">
